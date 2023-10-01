1923e73070bccc40bfbc252fdfb1acbf2e7e53fb
Eduardo Serpa <104046851+eduardoscavalcante@users.noreply.github.com>
2023-10-01 15:24:56 -0300

    Atualização

--- Projeto Compilador/AnalizadorSintatico.py
+++ Projeto Compilador/AnalizadorSintatico.py
@@ -16,7 +16,7 @@ class A_Sintatico:
             self.erro_sintatico()
 
     def programa_SOL(self):
-        self.pos += 1
+        self.match('programa_SOL')
         self.match('loop')
         self.vezes()
         self.sequencia()
@@ -31,26 +31,19 @@ class A_Sintatico:
         
 
     def sequencia(self):
-        if self.tokens[self.pos][1] == 'Present':
-            self.match('Present')
-            self.Present()
-        elif self.tokens[self.pos][1] == 'fases_EPIC':
+        if self.tokens[self.pos+3][1] == 'navegador':
             self.fases_EPIC()
-        else:
-            self.erro_sintatico()
+        elif
+            self.Present()
 
     def fases_EPIC(self):
-        self.match('Explore')
         self.Explore()
-        self.match('Present')
         self.Present()
-        self.match('Interact')
         self.Interact()
-        self.match('Critique')
         self.Critique()
         
     def Explore(self):
-        self.match('navegar')
+        self.navegar()
         self.tempo()
         self.match(';')
 
@@ -58,6 +51,13 @@ class A_Sintatico:
         choice = self.tokens[self.pos][1]
         if choice in ['visualizar_pdf', 'visualizar_vídeo', 'videoconferência']:
             self.match(choice)
+            if choice == 'visualizar_pdf'
+                self.visualizar_pdf()
+            elif choice == 'visualizar_vídeo'
+                self.visualizar_video()
+            elif choice == 'videoconferência'
+                self.videoconferencia()
+            
             self.tempo()
             self.match(';')
         else:
@@ -67,6 +67,13 @@ class A_Sintatico:
         choice = self.tokens[self.pos][1]
         if choice in ['whatsapp_web', 'email', 'videoconferência']:
             self.match(choice)
+            if choice == 'whatsapp_web'
+                self.whatsapp_web()
+            elif choice == 'email'
+                self.email()
+            elif choice == 'videoconferência'
+                self.videoconferencia()
+            
             self.tempo()
             self.match(';')
         else:
@@ -76,49 +83,45 @@ class A_Sintatico:
         choice = self.tokens[self.pos][1]
         if choice in ['whatsapp_web', 'email', 'videoconferência']:
             self.match(choice)
+            if choice == 'whatsapp_web'
+                self.whatsapp_web()
+            elif choice == 'email'
+                self.email()
+            elif choice == 'videoconferência'
+                self.videoconferencia()
+            
             self.tempo()
             self.match(';')
         else:
             self.erro_sintatico()
 
     def tempo(self):
-        if self.tokens[self.pos][1] in ['15_min', '20_min', '1_hora', '1_dia', '2_dias', 'sem limite']:
+        if self.tokens[self.pos][1] in ['15_min', '20_min', '1_hora', '1_dia', '2_dias', 'sem_limite']:
             self.match(self.tokens[self.pos][1])
         else:
             self.erro_sintatico()
     
     def navegar(self):
-        self.match('browser')
         self.browser()
     
     def visualizar_pdf(self):
-        self.match('browser')
         self.browser()
-        self.match('link_pdf')
         self.link_pdf()
     
     def visualizar_video(self):
-        self.match('browser')
         self.browser()
-        self.match('link_video')
         self.link_video()
     
     def videoconferencia(self):
-        self.match('browser')
         self.browser()
-        self.match('link_videoconferencia')
         self.link_videoconferencia()
     
     def whatsapp_web(self):
-        self.match('browser')
         self.browser()
-        self.match('link_whatsapp_web')
-        self.link_whatsapp_web
+        self.link_whatsapp_web()
     
     def email(self):
-        self.match('browser')
         self.browser()
-        self.match('link_email')
         self.link_email()
     
     def browser(self):
